3faecf519986487348dcd35e8600792c8fc74b07
VanyaBizhko <vanyabigko@gmail.com>
2024-05-15 20:47:46 +0300

    fin

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,7 @@
-import React, { useState, ChangeEvent} from "react";
-import  {InputProps}  from "./InputType";
+import React, { useState, ChangeEvent, KeyboardEvent } from "react";
+import { InputProps } from "./InputType";
 
-const Input: React.FC<InputProps> = ({ onSearch })=>{
+const Input: React.FC<InputProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -14,20 +14,32 @@ const Input: React.FC<InputProps> = ({ onSearch })=>{
     }
   };
 
+  const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearchClick();
+    }
+  };
+
   return (
-    <div className="flex justify-center gap-7 mb-8 ">
+    <div className="flex justify-center gap-7 mb-8">
       <input
         type="text"
         autoComplete="off"
         autoFocus
-      className="rounded-xl p-1.5 px-5 outline-0 font-medium"
+        className="rounded-xl p-1.5 px-5 outline-0 font-medium"
         placeholder="Search characters..."
         value={searchTerm}
         onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
       />
-      <button className="bg-sky-600 rounded-2xl px-3.5 font-medium" onClick={handleSearchClick}>Search</button>
+      <button
+        className="bg-sky-600 rounded-2xl px-3.5 font-medium"
+        onClick={handleSearchClick}
+      >
+        Search
+      </button>
     </div>
   );
-}
+};
 
 export default Input;
